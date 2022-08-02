f9131364e92c157ef1f10e814d97ad774920407b
vicentej20 <jorquerav07@gmail.com>
2022-08-01 21:51:54 -0400

    some improvements

--- FontEnd_Mentor/expenses-chart-component/src/Components/TotalMonth.jsx
+++ FontEnd_Mentor/expenses-chart-component/src/Components/TotalMonth.jsx
@@ -4,13 +4,13 @@ export default function TotalMonth({amount}){
     return (
 
         <div className="totalMonthRoot row container-fluid">
-            <div className="amountDiv col-6">
+            <div className="amountDiv col-8">
                 <p className="titleAmount">Total this month</p>
                 <h3 className="totalAmountCard">$ {amount * 2}</h3>
             </div>
-            <div className="percentageDiv col-6 m-0">
+            <div className="percentageDiv col-4 m-0">
                 <h4 className="percentegeAmount">+{+( ( +amount || 0 ) * 0.03 ).toFixed(2)}%</h4>
-                <p className="titleAmount">from last month</p>
+                <p className="titleAmount lastMonthTitle">from last month</p>
             </div>
         </div>
         
