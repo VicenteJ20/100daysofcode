416bc8b90ad3a4f77c29355a8b17ec1834dc1ca2
vicentej20 <jorquerav07@gmail.com>
2022-08-04 18:56:57 -0400

    some improvements

--- FontEnd_Mentor/calculator-app/src/App.js
+++ FontEnd_Mentor/calculator-app/src/App.js
@@ -12,82 +12,161 @@ export default function App(){
     setSelectedTheme(theme);
   }
 
+  // THEME 1
   const theme1 = {
-    color: 'hsl(0, 0, 90%)',
     backgroundColor: 'hsl(222, 26%, 31%)',
     transition: '1s',
   }
 
-  const theme2 = {
-    color: 'hsl(60, 10%, 19%)',
-    backgroundColor: 'hsl(0, 0%, 90%)',
-    transition: '1s',
-  }
-
-  const theme3 = {
-    color: 'hsl(0, 0, 100%)',
-    backgroundColor: 'hsl(268, 75%, 9%)',
-    transition: '1s',
-  }
-
-  const buttonStyleStandar = {
-    height: '2rem',
-    width: '2rem',
-    border: 'none',
-    borderRadius: '50%',
-    backgroundColor: 'transparent',
-  }
-
   const buttonStyleSelected = {
-    height: '2rem',
-    width: '2rem',
     border: 'none',
     borderRadius: '50%',
-    backgroundColor: 'orange',
+    backgroundColor: 'hsl(6, 63%, 50%)',
   }
 
   const backgroundTopTheme = {
     backgroundColor: 'hsl(223, 31%, 20%)',
-    fontSize: '0.2rem',
-    padding: '0.6rem'
+    padding: '0.2rem',
+    transition: '1s',
+
   }
 
   const titleStyleTheme1 = {
     color: 'hsl(30, 25%, 89%)',
-    fontWeight: '700'
+    fontWeight: '700',
+    transition: '1s',
   }
 
+  const buttonStyleStandar = {
+    border: 'none',
+    backgroundColor: 'transparent',
+  }
+
+  const footerTheme1 = {
+    color: 'hsl(28, 16%, 65%)',
+    fontWeight: '600',
+    transition: '1s',
+  }
+
+  const linkTheme1 = {
+    color: 'hsl(30, 25%, 89%)',
+    fontWeight: '500',
+  }
+
+
+  // THEME 2
+  const theme2 = {
+    backgroundColor: 'hsl(0, 0%, 90%)',
+    transition: '1s',
+  }
+
+  const buttonStyleSelected2 = {
+    border: 'none',
+    borderRadius: '50%',
+    backgroundColor: 'hsl(25, 98%, 40%)',
+  }
+
+  const titleStyleTheme2 = {
+    color: 'hsl(60, 10%, 19%)',
+    fontWeight: '700',
+    transition: '1s',
+  }
+
+  const backgroundTopTheme2 = {
+    backgroundColor: 'hsl(0, 5%, 81%)',
+    padding: '0.2rem',
+    transition: '1s',
+  }
+
+  const footerTheme2 = {
+    color: 'hsl(185, 58%, 25%)',
+    fontWeight: '600',
+    transition: '1s',
+  }
+
+  const linkTheme2 = {
+    color: 'hsl(25, 98%, 40%)',
+    fontWeight: '500',
+  }  
+
+  // THEME 3
+
+  const theme3 = {
+    backgroundColor: 'hsl(268, 75%, 9%)',
+    transition: '1s',
+  }
+
+  const backgroundTopTheme3 = {
+    backgroundColor: 'hsl(268, 47%, 21%)',
+    padding: '0.2rem',
+    transition: '1s',
+  }
+
+  const buttonStyleSelected3 = {
+    border: 'none',
+    borderRadius: '50%',
+    backgroundColor: 'hsl(177, 92%, 70%)',
+  }
+
+  const titleStyleTheme3 = {
+    color: 'hsl(52, 100%, 62%)',
+    fontWeight: '700',
+    transition: '1s',
+  }
+
+  const footerTheme3 = {
+    color: 'hsl(52, 100%, 62%)',
+    fontWeight: '600',
+    transition: '1s',
+  }
+
+  const linkTheme3 = {
+    color: 'hsl(176, 100%, 44%)',
+    fontWeight: '500',
+  }
   
   return (
     <div className='App row container-fluid justify-content-center align-items-center' style={selectTheme === 1 ? theme1 : selectTheme === 2 ? theme2 : theme3}>
-      <div className='AppRootContent col-lg-4 col-md-6 col-11'>
-        <div className='headerContent row container-fluid align-items-center'>
-          <div className='col-8 mt-5'>
-            <h1 style={titleStyleTheme1} className='title'>Calc</h1>
+      <div className='AppRootContent col-lg-4 col-md-8 col-12'>
+        <div className='headerContent justify-content-center row container-fluid align-items-center'>
+          <div className='col-md-8 col-5 mt-5'>
+            <h1 style={selectTheme === 1 ? titleStyleTheme1 : selectTheme === 2 ? titleStyleTheme2 : titleStyleTheme3} className='title'>Calc</h1>
           </div>
-          <div className='themeButtonsDiv col-4 row container-fluid'>
-            <div className='buttonDiv col-4 p-0'>
-              <p>1</p>
-              <div style={backgroundTopTheme} className='backgroundButton' id='bck1'><button className='buttonTheme' value={selectTheme ? 1 : 1} style={selectTheme === 1 ? buttonStyleSelected : buttonStyleStandar} onClick={handleThemeFilter}></button></div>
+          <div className='themeButtonsDiv col-md-4 col-6 gap-3 row container-fluid'>
+            <div className='p-0 col-4 align-items-end d-flex'>
+              <h3 className='titleTheme' style={selectTheme === 1 ? titleStyleTheme1 : selectTheme === 2 ? titleStyleTheme2 : titleStyleTheme3}>THEME</h3>
             </div>
-            <div className='buttonDiv col-4 p-0'>
-
-              <p>2</p>
-              <div style={backgroundTopTheme}className='backgroundButton'>
-                <button className='buttonTheme' value={selectTheme ? 2 : 2} style={selectTheme === 2 ? buttonStyleSelected : buttonStyleStandar} onClick={handleThemeFilter}></button>
+            <div className='col-md-8 col-8 row container-fluid'>
+              <div className='buttonDiv col-4 p-0'>
+                <p style={selectTheme === 1 ? titleStyleTheme1 : selectTheme === 2 ? titleStyleTheme2 : titleStyleTheme3}>1</p>
+                <div style={selectTheme === 1 ? backgroundTopTheme : selectTheme === 2 ? backgroundTopTheme2 : backgroundTopTheme3} className='backgroundButton' id='bck1'>
+                  <button className='buttonTheme' id='buttonTheme' value={selectTheme ? 1 : 1} style={selectTheme === 1 ? buttonStyleSelected : buttonStyleStandar} onClick={handleThemeFilter}></button>
+                </div>
               </div>
-              
-            </div>
-            <div className='buttonDiv col-4 p-0'>
-              <p>3</p>
-              <div style={backgroundTopTheme}className='backgroundButton ' id='bck3'>
-                <button className='buttonTheme' value={selectTheme ? 3 : 3} style={selectTheme === 3 ? buttonStyleSelected : buttonStyleStandar} onClick={handleThemeFilter}></button>
+              <div className='buttonDiv col-4 p-0'>
+
+                <p style={selectTheme === 1 ? titleStyleTheme1 : selectTheme === 2 ? titleStyleTheme2 : titleStyleTheme3}>2</p>
+                <div style={selectTheme === 1 ? backgroundTopTheme : selectTheme === 2 ? backgroundTopTheme2 : backgroundTopTheme3} className='backgroundButton'>
+                  <button className='buttonTheme' value={selectTheme ? 2 : 2} style={selectTheme === 2 ? buttonStyleSelected2 : buttonStyleStandar} onClick={handleThemeFilter}></button>
+                </div>
+                
+              </div>
+              <div className='buttonDiv col-4 p-0'>
+                <p style={selectTheme === 1 ? titleStyleTheme1 : selectTheme === 2 ? titleStyleTheme2 : titleStyleTheme3}>3</p>
+                <div style={selectTheme === 1 ? backgroundTopTheme : selectTheme === 2 ? backgroundTopTheme2 : backgroundTopTheme3} className='backgroundButton ' id='bck3'>
+                  <button className='buttonTheme' value={selectTheme ? 3 : 3} style={selectTheme === 3 ? buttonStyleSelected3 : buttonStyleStandar} onClick={handleThemeFilter}></button>
+                </div>
               </div>
             </div>
           </div>
         </div>
         <div className='calculatorDiv'>
           <Calculator theme={selectTheme}/>
+        </div>
+        <div className='footer'>
+          <footer style={selectTheme === 1 ? footerTheme1 : selectTheme === 2 ? footerTheme2 : footerTheme3}>
+          Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel='noreferrer' style={selectTheme === 1 ? linkTheme1 : selectTheme === 2 ? linkTheme2 : linkTheme3}>Frontend Mentor</a>. Coded by <a href="https://vicentejorquera.netlify.app" rel='noreferrer' style={selectTheme === 1 ? linkTheme1: selectTheme === 2 ? linkTheme2 : linkTheme3}>Vicente Jorquera</a>.
+          </footer>
         </div>
       </div>
     </div>
--- FontEnd_Mentor/calculator-app/src/Components/calculator.jsx
+++ FontEnd_Mentor/calculator-app/src/Components/calculator.jsx
@@ -9,10 +9,12 @@ export default function Calculator({theme}){
 
     const buttonsTheme1Background = {
         backgroundColor: 'hsl(223, 31%, 20%)',
+        transition: '1s',
     }
 
     const displayTheme1 = {
         backgroundColor: 'hsl(224, 36%, 15%)',
+        transition: '1s',
     }
 
     const buttonTheme1 = {
@@ -40,32 +42,111 @@ export default function Calculator({theme}){
         justifyContent: 'center',
         paddingBottom: '0.7rem'
       }
+
     
+      //THEME 2
+    
+    const displayTheme2 = {
+        backgroundColor: 'hsl(0, 0%, 93%)',
+        transition: '1s',
+        color: 'hsl(60, 10%, 19%)',
+    }
+
+    const buttonsTheme2Background = {
+        backgroundColor: 'hsl(0, 5%, 81%)',
+        transition: '1s',
+    }
+
+    const delTheme2 = {
+        boxShadow: '0 0.5rem 0 0 hsl(185, 58%, 25%)',
+        transition: '0.5s',
+        backgroundColor: 'hsl(185, 42%, 37%)',
+        color: 'hsl(45, 7%, 89%)',
+        textTransform: 'uppercase',
+    }
+
+    const buttonTheme2 = {
+        boxShadow: '0 0.5rem 0 0 hsl(35, 11%, 61%)',
+        transition: '0.5s',
+        backgroundColor: 'hsl(45, 7%, 89%)',
+    }
+
+    const equalButtonTheme2 = {
+        boxShadow: '0 0.5rem 0 0 hsl(25, 99%, 27%)',
+        transition: '0.5s',
+        backgroundColor: 'hsl(25, 98%, 40%)',
+        color: 'hsl(45, 7%, 89%)',
+        fontSize: '3rem',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingBottom: '0.7rem'
+      }
+
+      // Theme 3
+      const displayTheme3 = {
+        backgroundColor: 'hsl(268, 71%, 12%)',
+        color: 'hsl(52, 100%, 62%)',
+        transition: '1s',
+    }
+
+    const buttonsTheme3Background = {
+        backgroundColor: 'hsl(268, 71%, 12%)',
+        transition: '1s',
+    }
+
+    const delTheme3 = {
+        boxShadow: '0 0.5rem 0 0 hsl(285, 91%, 52%)',
+        transition: '0.5s',
+        backgroundColor: 'hsl(290, 70%, 36%)',
+        color: 'hsl(45, 7%, 89%)',
+        textTransform: 'uppercase',
+    }
+
+    const buttonTheme3 = {
+        color: 'hsl(52, 100%, 62%)',
+        boxShadow: '0 0.5rem 0 0 hsl(290, 70%, 36%)',
+        transition: '0.5s',
+        backgroundColor: 'hsl(268, 47%, 21%)',
+    }
+
+    const equalButtonTheme3 = {
+        boxShadow: '0 0.5rem 0 0 hsl(176, 100%, 44%)',
+        transition: '0.5s',
+        backgroundColor: 'hsl(177, 92%, 70%)',
+        color: 'hsl(268, 47%, 21%)',
+        fontSize: '3rem',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingBottom: '0.7rem'
+      }
+
 
     return (
         <div  className="CalculatorRoot container-fluid mt-5">
-            <div style={displayTheme1} className='rootDisplay row container-fluid p-4'>
+            <div style={theme === 1 ? displayTheme1 : theme === 2 ? displayTheme2 : displayTheme3} className='rootDisplay row container-fluid p-4'>
                 {number}
             </div>
-            <div style={buttonsTheme1Background} className='buttonDivCalc row container-fluid'>
-                <button style={buttonTheme1} className='buttonCalc col-2' onClick={(e) => {setNumber(number + e.target.value)}} value={7} key={7}>7</button>
-                <button style={buttonTheme1} className='buttonCalc col-2' onClick={(e) => {setNumber(number + e.target.value)}} value={8} key={8}>8</button>
-                <button style={buttonTheme1} className='buttonCalc col-2' onClick={(e) => {setNumber(number + e.target.value)}} value={9} key={9}>9</button>
-                <button style={delTheme1} className='buttonCalc col-2' onClick={() => setNumber(number.substr(0, number.length - 1))} value={'del'}>del</button>
-                <button style={buttonTheme1} className='buttonCalc col-2' onClick={(e) => {setNumber(number + e.target.value)}} value={4} key={4}>4</button>
-                <button style={buttonTheme1} className='buttonCalc col-2' onClick={(e) => {setNumber(number + e.target.value)}} value={5} key={5}>5</button>
-                <button style={buttonTheme1} className='buttonCalc col-2' onClick={(e) => {setNumber(number + e.target.value)}} value={6} key={6}>6</button>
-                <button style={buttonTheme1} className='buttonCalc col-2' onClick={(e) => setNumber(number + e.target.value)} value={'+'}>+</button>
-                <button style={buttonTheme1} className='buttonCalc col-2' onClick={(e) => {setNumber(number + e.target.value)}} value={1} key={1}>1</button>
-                <button style={buttonTheme1} className='buttonCalc col-2' onClick={(e) => {setNumber(number + e.target.value)}} value={2} key={2}>2</button>
-                <button style={buttonTheme1} className='buttonCalc col-2' onClick={(e) => {setNumber(number + e.target.value)}} value={3} key={3}>3</button>
-                <button style={buttonTheme1} className='buttonCalc col-2' onClick={(e) => setNumber(number + e.target.value)} value={'-'}>-</button>
-                <button style={buttonTheme1} className='buttonCalc col-2' onClick={(e) => {setNumber(number + e.target.value)}} value={'.'} key={'.'}>.</button>
-                <button style={buttonTheme1} className='buttonCalc col-2' onClick={(e) => {setNumber(number + e.target.value)}} value={0} key={0}>0</button>
-                <button style={buttonTheme1} className='buttonCalc col-2' onClick={(e) => {setNumber(number + e.target.value)}} value={'/'} key={'/'}>/</button>
-                <button style={buttonTheme1} className='buttonCalc col-2' onClick={(e) => setNumber(number + e.target.value)}  value={'*'}>x</button>
-                <button style={delTheme1} className='buttonCalc col-4' onClick={() => setNumber(' ')} >reset</button>
-                <button style={equalButtonTheme1} className='buttonCalc col-4 ms-4' onClick={
+            <div style={theme === 1 ? buttonsTheme1Background : theme === 2 ? buttonsTheme2Background : buttonsTheme3Background} className='buttonDivCalc row container-fluid'>
+                <button style={theme === 1 ? buttonTheme1 : theme === 2 ? buttonTheme2 : buttonTheme3} className='buttonCalc col-2' onClick={(e) => {setNumber(number + e.target.value)}} value={7} key={7}>7</button>
+                <button style={theme === 1 ? buttonTheme1 : theme === 2 ? buttonTheme2 : buttonTheme3} className='buttonCalc col-2' onClick={(e) => {setNumber(number + e.target.value)}} value={8} key={8}>8</button>
+                <button style={theme === 1 ? buttonTheme1 : theme === 2 ? buttonTheme2 : buttonTheme3} className='buttonCalc col-2' onClick={(e) => {setNumber(number + e.target.value)}} value={9} key={9}>9</button>
+                <button style={theme === 1 ? delTheme1 : theme === 2 ? delTheme2 : delTheme3} className='buttonCalc col-2' onClick={() => setNumber(number.substr(0, number.length - 1))} value={'del'}>del</button>
+                <button style={theme === 1 ? buttonTheme1 : theme === 2 ? buttonTheme2 : buttonTheme3} className='buttonCalc col-2' onClick={(e) => {setNumber(number + e.target.value)}} value={4} key={4}>4</button>
+                <button style={theme === 1 ? buttonTheme1 : theme === 2 ? buttonTheme2 : buttonTheme3} className='buttonCalc col-2' onClick={(e) => {setNumber(number + e.target.value)}} value={5} key={5}>5</button>
+                <button style={theme === 1 ? buttonTheme1 : theme === 2 ? buttonTheme2 : buttonTheme3} className='buttonCalc col-2' onClick={(e) => {setNumber(number + e.target.value)}} value={6} key={6}>6</button>
+                <button style={theme === 1 ? buttonTheme1 : theme === 2 ? buttonTheme2 : buttonTheme3} className='buttonCalc col-2' onClick={(e) => setNumber(number + e.target.value)} value={'+'}>+</button>
+                <button style={theme === 1 ? buttonTheme1 : theme === 2 ? buttonTheme2 : buttonTheme3} className='buttonCalc col-2' onClick={(e) => {setNumber(number + e.target.value)}} value={1} key={1}>1</button>
+                <button style={theme === 1 ? buttonTheme1 : theme === 2 ? buttonTheme2 : buttonTheme3} className='buttonCalc col-2' onClick={(e) => {setNumber(number + e.target.value)}} value={2} key={2}>2</button>
+                <button style={theme === 1 ? buttonTheme1 : theme === 2 ? buttonTheme2 : buttonTheme3} className='buttonCalc col-2' onClick={(e) => {setNumber(number + e.target.value)}} value={3} key={3}>3</button>
+                <button style={theme === 1 ? buttonTheme1 : theme === 2 ? buttonTheme2 : buttonTheme3} className='buttonCalc col-2' onClick={(e) => setNumber(number + e.target.value)} value={'-'}>-</button>
+                <button style={theme === 1 ? buttonTheme1 : theme === 2 ? buttonTheme2 : buttonTheme3} className='buttonCalc col-2' onClick={(e) => {setNumber(number + e.target.value)}} value={'.'} key={'.'}>.</button>
+                <button style={theme === 1 ? buttonTheme1 : theme === 2 ? buttonTheme2 : buttonTheme3} className='buttonCalc col-2' onClick={(e) => {setNumber(number + e.target.value)}} value={0} key={0}>0</button>
+                <button style={theme === 1 ? buttonTheme1 : theme === 2 ? buttonTheme2 : buttonTheme3} className='buttonCalc col-2' onClick={(e) => {setNumber(number + e.target.value)}} value={'/'} key={'/'}>/</button>
+                <button style={theme === 1 ? buttonTheme1 : theme === 2 ? buttonTheme2 : buttonTheme3} className='buttonCalc col-2' onClick={(e) => setNumber(number + e.target.value)}  value={'*'}>x</button>
+                <button style={theme === 1 ? delTheme1 : theme === 2 ? delTheme2 : delTheme3} className='buttonCalc col-4' onClick={() => setNumber(' ')} >reset</button>
+                <button style={theme === 1 ? equalButtonTheme1 : theme === 2 ? equalButtonTheme2 : equalButtonTheme3} className='buttonCalc col-4 ms-4' onClick={
                     (e) => {
                         try {
                             setNumber(
